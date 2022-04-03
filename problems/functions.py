054b7239e298add60f36ac50b977d9fd4d90be05
Luke Sprangers <sprangersluke@gmail.com>
2022-04-03 15:52:40 -0400

    Finish problems 1-10

--- problems/functions.py
+++ problems/functions.py
@@ -1,10 +1,11 @@
 import numpy as np
+import itertools
 
 
 def fibonacci(ceiling_limit):
     """
-    ceiling_limit (int): A number describing the max number to calculate sequence up to
-    Returns a list containing the fibonacci sequence for a specified ceiling_limit
+        ceiling_limit (int): A number describing the max number to calculate sequence up to
+        Returns a list containing the fibonacci sequence for a specified ceiling_limit
     """
     fib = [0, 1]
     while fib[-1] < ceiling_limit:
@@ -31,3 +32,96 @@ def prime_factors(n):
     return list(
         np.unique(prime_factorization(n))
     )
+
+
+def reverse_number(n):
+    """Reverse a number"""
+    rev = 0
+    while n > 0:
+        remainder = n % 10
+        rev = (rev * 10) + remainder
+        n = n // 10
+    return rev
+
+
+def is_palindrome(n):
+    """Check if a numer is a palindrome"""
+    rev = reverse_number(n)
+    return 1 if n == rev else 0
+
+
+def gcd(a, b):
+    """Greatest common denominator"""
+    return gcd(b, a % b) if b else a
+
+
+# Exploit lcm(a, b) * gcd(a, b) = a * b
+
+def lcm(a, b):
+    """Least common multiple"""
+    return (a * b) / gcd(a, b)
+
+
+def p_sieve():
+    """
+        Proposed sieve prime number generator from
+        https://stackoverflow.com/questions/2211990/how-to-implement-an-efficient-infinite-generator-of-prime-numbers-in-python/10733621#10733621
+    """
+    yield from [2, 3, 5, 7]
+    D = {}
+    ps = p_sieve()
+    next(ps)
+    p = next(ps)
+    assert p == 3
+    psq = p * p
+    for i in itertools.count(9, 2):
+        if i in D:  # composite
+            step = D.pop(i)
+        elif i < psq:  # prime
+            yield i
+            continue
+        else:  # composite, = p*p
+            assert i == psq
+            step = 2 * p
+            p = next(ps)
+            psq = p * p
+        i += step
+        while i in D:
+            i += step
+        D[i] = step
+
+
+def get_nth_prime(n):
+    idx = 0
+    prime_gen = p_sieve()
+    while idx < n:
+        pr_num = next(prime_gen)
+        idx += 1
+    return pr_num
+
+
+def naive_pythagorean_triplets(ceiling_limit):
+    c, m = 0, 2
+    pairs_list = []
+
+    # Limiting c would limit
+    # all a, b and c
+    while c < ceiling_limit:
+
+        # Now loop on n from 1 to m-1
+        for n in range(1, m):
+            a = m * m - n * n
+            b = 2 * m * n
+            c = m * m + n * n
+
+            # if c is greater than
+            # limit then break it
+            if c > ceiling_limit:
+                break
+
+            pairs_list.append((a, b, c))
+
+        m += 1
+
+    return pairs_list
+
--- problems/problems_1_10.py
+++ problems/problems_1_10.py
@@ -1,5 +1,6 @@
 from problems import functions as mf
 import numpy as np
+from functools import reduce
 
 # 1
 # Find the sum of all the multiples of 3 or 5 below 1000.
@@ -12,6 +13,7 @@ np.sum(ls_1)
 # 2
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million,
 # find the sum of the even-valued terms.
+
 ls_2 = mf.fibonacci(4_000_000)
 np.sum([i for i in ls_2 if i % 2 == 0])
 
@@ -20,15 +22,62 @@ np.sum([i for i in ls_2 if i % 2 == 0])
 np.max(mf.prime_factors(600_851_475_143))
 
 # 4
+# Find the largest palindrome made from the product of two 3-digit numbers.
+max_p = 0
+for i in range(999):
+    for j in range(999):
+        n = i * j
+        bool_pal = mf.is_palindrome(n)
+        if bool_pal and n > max_p:
+            max_p = n
 
 # 5
+# What is the smallest positive number that is evenly divisible by
+# all of the numbers from 1 to 20?
+#   Exploit LCM(x0,x1,x2) = LCM(x0,LCM(x1,x2))
+reduce(mf.lcm, range(2, 21))
 
 # 6
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+first_hundred = list(range(1, 101))
+sum_first_hundred = np.sum(first_hundred)
+abs(
+    np.sum([i * i for i in first_hundred]) -
+    (sum_first_hundred * sum_first_hundred)
+)
 
 # 7
+mf.get_nth_prime(10001)
 
 # 8
+# Find the thirteen adjacent digits (sliding window left right not up down)
+# in the 1000-digit number that have the greatest product. What is the value of this product?
+with open('./problems/helper_files/thousand_digit_number.txt', 'r') as f:
+    num_str = f.read()
+
+slider_len = 13
+max_num = 0
+for i in range(0, len(num_str) - slider_len):
+    num_sub = num_str[i:i + slider_len]
+    nums = [int(j) for j in num_sub]
+    this_prod = np.prod(nums)
+    max_num = this_prod if this_prod > max_num else max_num
 
 # 9
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+triples = mf.naive_pythagorean_triplets(1001)
+for t in triples:
+    if np.sum(t) == 1000:
+        print(np.product(t))
 
 # 10
+# Find the sum of all the primes below two million.
+p_gen = mf.p_sieve()
+prime_list = []
+while True:
+    next_prime = next(p_gen)
+    if next_prime > 2_000_000:
+        break
+    prime_list.append(next_prime)
+np.sum(prime_list)
